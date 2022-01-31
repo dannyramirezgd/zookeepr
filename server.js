a1287e00d98881941177114994407eeb5b6ab869
Danny Ramirez <danny.ramirezgd@gmail.com>
2022-01-31 14:41:51 -0500

    added findbyID route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { animals } = require('./data/animals.json')
 
 const express = require('express');
+const res = require('express/lib/response');
 
 const PORT = process.env.PORT || 3001; 
 
@@ -43,12 +44,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+})
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result)
+    } else {
+        res.sendStatus(404);
+    }  
 })
 
 app.listen(PORT, () => {
